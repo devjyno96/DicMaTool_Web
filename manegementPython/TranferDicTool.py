6bb3739788f713f7f9e8d012eebd997e317be668
jyno <jinho9613@naver.com>
2020-07-23 18:23:21 +0900

    create func search, generateKsDicfile
    create function Transfer Search
    Search need func generateKSDicFile
    create generateKSDicFile

--- manegementPython/TranferDicTool.py
+++ manegementPython/TranferDicTool.py
@@ -1,10 +1,78 @@
 class transferDic :
+    KS_DIC_FOLDER = "KS-DICT"
+    N_DIC_FOLDER = "N-DICT"
+    CONVERT_POS = {
+        "noun": "nn",
+        "verb": "vb",
+        "adj": "adj",
+        "adv": "adv",
+        "pron": "prn",
+        "conj": "cnj",
+        "prep": "prp"
+    }
+    FOLDER_Name = {
+            "nar" : "Airplane",
+            "arc" : "Architecture",
+            "atm" : "Atomic",
+            "ato" : "Auto",
+            "bio" : "Bio",
+            "jco" : "Computer-J",
+            "nco" : "Computer-N",
+            "nee" : "Economy-N",
+            "vee" : "Economy-V",
+            "ind" : "Industry",
+            "med" : "Medical",
+            "tac" : "Military",
+            "pat" : "Patent",
+            "npo" : "Politics",
+            "shp" : "Ship",
+            "nsp" : "Sports-N",
+            "vsp" : "Sports-V",
+            "tel" : "Telecom"
+        }
+
     def __init__(self):
         print("init")
 
 
     def Search(self, data):
-        print(data['word'] + " : " + data['generics'] + " : " + data['domains'])
+        word = data['word']
+        generic = data['generics']
+        domain = data['domains'][0:3]
+        POS = ""
+        # generic is none  pos is domain, else pos is generic
+        if len(generic) == 0 :
+            POS = domain
+        else :
+            POS = generic
+
+        newDomainPOS = ""
+
+        if POS not in self.CONVERT_POS :
+            newDomainPOS = POS
+        else :
+            newDomainPOS = self.CONVERT_POS[POS]
+
+        firstLetter = word[0].lower()
+
+        folderName = ""
+        if POS not in self.FOLDER_Name :
+            folderName = "General"
+        else :
+            folderName = self.FOLDER_Name[POS]
+
+        KSDicFullFileName = self.KS_DIC_FOLDER + "\\" + folderName + "\\"
+        NDicFullFileName = self.N_DIC_FOLDER + "\\" + folderName + "\\"
+        fileName = "dic"
+        if newDomainPOS not in ['pron', 'prep', 'conj']:
+            fileName += firstLetter
+        fileName += '.'
+        fileName += newDomainPOS
+        NDicFullFileName += fileName
+        KSDicFullFileName += fileName
+        KSDicFullFileName += '.txt'
+        print(NDicFullFileName)
+        print(KSDicFullFileName)
 
 
     def Update(self, word, genericPOS, domainPOS):
@@ -17,3 +85,20 @@ class transferDic :
     def makeDomainDB(self, word, genericPOS, domainPOS):
         print("transferMakeDomainDB")
 
+    def generateKSDicFile(self, folderName, fileName):
+        alterFullFileName = self.N_DIC_FOLDER + "\\" + folderName + "\\" + fileName
+        cnExe = "EXE\\cn.exe"
+        cnArgument = "-nc " + alterFullFileName +  " " + fileName + ".jh"
+        # process run cn.exe (argument is cnArgument)
+
+        ksExe = "EXE\\kscode.exe"
+        ksArgument = "-jk " + fileName + ".jh" + self.KS_DIC_FOLDER + "\\" + folderName + "\\" + fileName +'.txt'
+        # process run kscode.exe (argument is ksArgument)
+
+
+        print("generateKsDicFile")
+
+if __name__ == "__main__" :
+    testClass = transferDic()
+    testData = {'word': 'test', 'generics': '', 'domains': 'atm (atomic)'}
+    testClass.Search(testData)
