c76debe572f9ccf88835dca1a18ea7270c482a22
jyno <jinho9613@naver.com>
2020-08-06 18:13:53 +0900

    create Lex Page JS

---
+++ static/js/lex.js
@@ -0,0 +1,209 @@
+let dom = {
+    lexWordInput : null,
+    lexSearchButton : null,
+    lexClearButton : null,
+    lexUpdateButton : null,
+    lexMakeLexicalDBButton : null,
+    lexDicInfoDetails : null,
+
+
+    probWordInput : null,
+    probSearchButton : null,
+    probClearButton : null,
+    probUpdateButton : null,
+    probMakeLexicalDBButton : null,
+    probDicInfoDetails : null
+};
+
+/**
+ * body onload = "init()"
+ */
+function init() {
+    getDomReferences();
+    registerListeners();
+}
+
+/**
+ * dom 객체 레퍼런스를 가져온다.
+ */
+function getDomReferences() {
+    dom.lexWordInput = document.getElementById("lexWordInput");
+    dom.lexSearchButton = document.getElementById("lexSearchButton");
+    dom.lexClearButton = document.getElementById("lexClearButton");
+    dom.lexUpdateButton = document.getElementById("lexUpdateButton");
+    dom.lexMakeLexicalDBButton = document.getElementById("MakeLexicalButton");
+    dom.lexDicInfoDetails = document.getElementById("lexInfoDetails");
+
+    dom.probWordInput = document.getElementById("probWordInput");
+    dom.probSearchButton = document.getElementById("probSearchButton");
+    dom.probClearButton = document.getElementById("probClearButton");
+    dom.probUpdateButton = document.getElementById("probUpdateButton");
+    dom.probMakeLexicalDBButton = document.getElementById("MakeProbButton");
+    dom.probDicInfoDetails = document.getElementById("probInfoDetails");
+}
+
+/**
+ * 이벤트 리스너를 등록한다.
+ */
+function registerListeners() {
+
+    dom.lexSearchButton.addEventListener("click", onMakeDomainDBButton);
+    dom.lexClearButton.addEventListener("click", onLexClearButtonClick);
+    dom.lexUpdateButton.addEventListener("click", onMakeDomainDBButton);
+    dom.lexMakeLexicalDBButton.addEventListener("click", onMakeDomainDBButton);
+
+    dom.probSearchButton.addEventListener("click", onMakeDomainDBButton);
+    dom.probClearButton.addEventListener("click", onProbClearButtonClick);
+    dom.probUpdateButton.addEventListener("click", onMakeDomainDBButton);
+    dom.probMakeLexicalDBButton.addEventListener("click", onMakeDomainDBButton);
+
+}
+// input values initialize
+function onLexClearButtonClick(){
+    dom.lexWordInput.value = null;
+    dom.lexDicInfoDetails.value = null;
+}
+
+// input values initialize
+function onProbClearButtonClick(){
+    dom.probWordInput.value = null;
+    dom.probDicInfoDetails.value = null;
+}
+
+
+function onSearchButtonClick(){
+    postSearch();
+}
+
+
+function onUpdateButtonClick(){
+    postUpdate();
+}
+
+
+function onMakeGenericDBButton(){
+    postMakeGenericDB();
+}
+
+function onMakeDomainDBButton(){
+    postMakeDomainDB();
+}
+
+//Post function
+//post - word, generic, domain
+//response - dicInfoLeft, Right
+function postSearch() {
+    var data = {
+        word : dom.wordInput.value,
+        generics : dom.genericPOSInput.value,
+        domains : dom.domainPOSInput.value
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/postSearch",
+        data: JSON.stringify(data),
+        dataType:'json' ,
+        contentType: "application/json",
+        success: function (response) {
+            // if error has data => show error msg alert
+            dom.dicInfoDetails.value = response.result;
+
+            if (typeof(response.errors) != "undefined" && response.errors.length != 0 ) {
+                alert((response.errors));
+                dom.dicInfoDetails.value = response.errors;
+            }
+            // dom.dicInfoDetailsLeft.value = response.dicInfoLeft;
+            //dom.dicInfoDetailsRight.value = response.dicInfoRight;
+            // text.posTagged = response.result;
+            // dom.dicInfoDetailsLeft.innerHTML = text.posTagged;
+
+        }
+    });
+}
+
+function postUpdate() {
+    var data = {
+        word : dom.wordInput.value,
+        generics : dom.genericPOSInput.value,
+        domains : dom.domainPOSInput.value,
+        updateText : dom.dicInfoDetails.value
+    };
+    $.ajax({
+        type: "POST",
+        url: "/postUpdate",
+        data: JSON.stringify(data),
+        dataType:'json' ,
+        contentType: "application/json",
+        success: function (response) {
+            // if error has data => show error msg alert
+            alert(response.result)
+            if (typeof(response.error) == 1) {
+                alert((response.message));
+                dom.dicInfoDetails.value = "";
+            }
+            // dom.dicInfoDetailsLeft.value = response.dicInfoLeft;
+            //dom.dicInfoDetailsRight.value = response.dicInfoRight;
+            // text.posTagged = response.result;
+            // dom.dicInfoDetailsLeft.innerHTML = text.posTagged;
+
+        }
+    });
+}
+
+
+function postMakeGenericDB() {
+
+    $.ajax({
+        type: "POST",
+        url: "/postMakeGenericDB",
+        data: JSON.stringify({}),
+        dataType:'json' ,
+        contentType: "application/json",
+        success: function (response) {
+            // if error has data => show error msg alert
+            dom.dicInfoDetails.value = response.result;
+/*
+            if (typeof(response.errors) != "undefined" && response.errors.length != 0 ) {
+                alert((response.errors));
+                dom.dicInfoDetails.value = response.errors;
+            }
+            // dom.dicInfoDetailsLeft.value = response.dicInfoLeft;
+            //dom.dicInfoDetailsRight.value = response.dicInfoRight;
+            // text.posTagged = response.result;
+            // dom.dicInfoDetailsLeft.innerHTML = text.posTagged;
+*/
+        }
+    });
+}
+
+function postMakeDomainDB() {
+    var data = {
+        word : dom.wordInput.value,
+        generics : dom.genericPOSInput.value,
+        domains : dom.domainPOSInput.value
+    };
+    $.ajax({
+        type: "POST",
+        url: "/postMakeDomainDB",
+        data: JSON.stringify(data),
+        dataType:'json' ,
+        contentType: "application/json",
+        success: function (response) {
+            // if error has data => show error msg alert
+            alert(response.result);
+/*
+            if (typeof(response.errors) != "undefined" && response.errors.length != 0 ) {
+                alert((response.errors));
+                dom.dicInfoDetails.value = response.errors;
+            }
+            // dom.dicInfoDetailsLeft.value = response.dicInfoLeft;
+            //dom.dicInfoDetailsRight.value = response.dicInfoRight;
+            // text.posTagged = response.result;
+            // dom.dicInfoDetailsLeft.innerHTML = text.posTagged;
+*/
+        }
+    });
+}
+
+
