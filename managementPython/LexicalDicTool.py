9d3bce6e5f739dbb7fc625a212dd441eb1646fac
jyno <jinho9613@naver.com>
2020-08-09 01:03:06 +0900

    lex, prob search complete

--- managementPython/LexicalDicTool.py
+++ managementPython/LexicalDicTool.py
@@ -2,26 +2,13 @@ import subprocess
 import os
 
 
-class lexicalDic :
+class lexicalDic:
     KS_DIC_FOLDER = "KS-DICT"
     N_DIC_FOLDER = "N-DICT"
+
+    LEX_DIC = "LEX-DIC\\base.dic"
+    LEX_PROB_DIC = "LEX-DIC\\lpf.lst"
     RELATIVE_PATH = "\\DicMaTool_Web\\managementPython\\EXE\\"
-    """
-    def __init__(self):
-        print("init")
-    """
-
-    def lexSearch(self, data):
-        print("lex Search")
-
-    def probSearch(self, data):
-        print("lex Search")
-
-    def lexUpdate(self, data):
-        print("lex Search")
-
-    def probUpdate(self, data):
-        print("lex Search")
 
     def makeLexicalDB(self):
         resultDict = {"result": "", "error": 0}
@@ -60,8 +47,95 @@ class lexicalDic :
         resultDict['result'] = "Lexical Prob Dictionary DB is created !"
         return resultDict
 
+
+    def lexFileSearch(self, data):
+        resultDict = {"result": "", "error": 0, "message": "", "file": None}
+        # generic is none  pos is domain, else pos is generic
+
+        if not os.path.isfile(self.RELATIVE_PATH + self.LEX_DIC):
+            resultDict['error'] = 1
+            resultDict["message"] = self.LEX_DIC + " doesn't exist !!!"
+            return resultDict
+        resultDict['file'] = open(self.RELATIVE_PATH + self.LEX_DIC)
+        return resultDict
+
+
+    def lexSearch(self, data):
+        resultDict = self.lexFileSearch(data)
+        resultDict.update({"foundLine": 0})
+
+        if resultDict['error'] == 1:
+            return resultDict
+
+        word = data['word']
+
+        if len(word) == 0:
+            resultDict['error'] = 1
+            resultDict["message"] = "No Word Specified !!!"
+            return resultDict
+
+        allLines = resultDict['file'].readlines()
+        keywordAllLines = [items.split(',')[0] for items in allLines] # keyword extraction
+
+        if word in keywordAllLines :
+            resultDict['foundLine'] = keywordAllLines.index(word)
+            resultDict['result'] = allLines[resultDict['foundLine']]
+        else :
+            resultDict["error"] = 1
+            resultDict["message"] = word + " is not in dictionary"
+            return resultDict
+
+        return resultDict
+
+
+    def probFileSearch(self):
+        resultDict = {"result": "", "error": 0, "message": "", "file": None}
+        # generic is none  pos is domain, else pos is generic
+
+        if not os.path.isfile(self.RELATIVE_PATH + self.LEX_PROB_DIC):
+            resultDict['error'] = 1
+            resultDict["message"] = self.LEX_PROB_DIC + " doesn't exist !!!"
+            return resultDict
+        resultDict['file'] = open(self.RELATIVE_PATH + self.LEX_PROB_DIC)
+        return resultDict
+
+
+    def probSearch(self, data):
+        resultDict = self.probFileSearch()
+        resultDict.update({"foundLine": 0})
+
+        if resultDict['error'] == 1:
+            return resultDict
+
+        word = data['word']
+
+        if len(word) == 0:
+            resultDict['error'] = 1
+            resultDict["message"] = "No Word Specified !!!"
+            return resultDict
+
+        allLines = resultDict['file'].readlines()
+        keywordAllLines = [items.split(' ')[0] for items in allLines]  # keyword extraction
+
+        if word in keywordAllLines:
+            resultDict['foundLine'] = keywordAllLines.index(word)
+            resultDict['result'] = allLines[resultDict['foundLine']]
+        else:
+            resultDict["error"] = 1
+            resultDict["message"] = word + " is not in dictionary"
+            return resultDict
+
+        return resultDict
+
+
+    def lexUpdate(self, data):
+        print("lex Search")
+
+    def probUpdate(self, data):
+        print("lex Search")
+
     def openDicFile(self, data):
-        resultDict = {"result": "", "error": 0, "message" : "",  "file" : None }
+        resultDict = {"result": "", "error": 0, "message": "", "file": None}
         word = data['word']
         generic = data['generics']
         domain = data['domains'][0:3]  # POS = 3 characters
@@ -133,23 +207,22 @@ class lexicalDic :
         resultDict['file'] = open(relativePath + KSDicFullFileName)
         return resultDict
 
-
     def Search(self, data):
         word = data['word']
 
         key = "\"" + word + "\""
 
         resultDict = self.openDicFile(data)
-        resultDict.update({ "foundLine" : 0 , "nextEntryLine" : 0 })
-        if resultDict['error'] == 1 :
+        resultDict.update({"foundLine": 0, "nextEntryLine": 0})
+        if resultDict['error'] == 1:
             return resultDict
         # print(allReadLines)
 
         allLines = resultDict['file'].readlines()
         index = 0
-        #Is key in the list?
-        if key+"\n" in allLines:
-            index = allLines.index(key+"\n")
+        # Is key in the list?
+        if key + "\n" in allLines:
+            index = allLines.index(key + "\n")
         else:
             # print(word + " is not in dictionary")
             errorMSG = word + " is not in dictionary"
@@ -162,39 +235,36 @@ class lexicalDic :
         resultDict['result'] = allLines[index]
 
         length = 0
-        for reads in allLines[index+1:] :
-            if(reads[0] == '"'):
+        for reads in allLines[index + 1:]:
+            if (reads[0] == '"'):
                 break
-            else :
+            else:
                 resultDict['result'] += reads
                 length += 1
         resultDict['nextEntryLine'] = length + 1
 
         return resultDict
 
-
     def Update(self, replaceData):
         searchResult = self.Search(replaceData)
-        searchResult.update({"result" : "", "errors" : ""})
-
+        searchResult.update({"result": "", "errors": ""})
 
         # file offset reset
         searchResult['file'].seek(0)
         allLines = searchResult['file'].readlines()
         updateData = []
 
-        #수정 전의 부분
+        # 수정 전의 부분
         updateData += allLines[: searchResult['foundLine'] - 1]
-        #수정 부분
-        for item in replaceData['updateText'].split("\n") :
-            updateData.append(item + "\n") # split 과정중 사라진 \n을 붙여줌
-        #수정 이후 부분
+        # 수정 부분
+        for item in replaceData['updateText'].split("\n"):
+            updateData.append(item + "\n")  # split 과정중 사라진 \n을 붙여줌
+        # 수정 이후 부분
         updateData += allLines[searchResult['foundLine'] + searchResult['nextEntryLine']:]
 
         domain = replaceData['domains']
         generic = replaceData['generics']
         word = replaceData['word']
-
 
         if len(generic) == 0:
             POS = domain
@@ -228,7 +298,6 @@ class lexicalDic :
 
         relativePath = "\\DicMaTool_Web\\managementPython\\EXE\\"
 
-
         txtFileName = "KS-DICT2\\" + folderName + "\\" + fileName + ".txt"
         txtFile = open(relativePath + txtFileName, 'w')
         txtFile.write("".join(updateData))
@@ -247,13 +316,13 @@ class lexicalDic :
         # cnArgument = "-nc " + alterFullFileName +  " " + fileName + ".jh"
         # process run cn.exe (argument is cnArgument)
         subprocess.run([relativePath + cnExe, "-cn", relativePath + tmpFileName,
-                        relativePath+ "N-DICT\\" + folderName + "\\" + fileName])
+                        relativePath + "N-DICT\\" + folderName + "\\" + fileName])
 
         # sTxtFileName + " is created" + Environment.NewLine + "N-DICT\\" + sFolderName + "\\" + sFileName + " is created")
-        replaceData['result'] = txtFileName + " is created\n" + "N-DICT\\" + folderName + "\\" + fileName + " is created"
+        replaceData[
+            'result'] = txtFileName + " is created\n" + "N-DICT\\" + folderName + "\\" + fileName + " is created"
 
         return replaceData
-
 
     def generateKSDicFile(self, folderName, fileName):
         alterFullFileName = self.N_DIC_FOLDER + "\\" + folderName + "\\" + fileName
@@ -262,23 +331,23 @@ class lexicalDic :
         cnExe = "cn.exe"
         # cnArgument = "-nc " + alterFullFileName +  " " + fileName + ".jh"
         # process run cn.exe (argument is cnArgument)
-        subprocess.run([relativePath + cnExe, "-nc", relativePath + alterFullFileName,  relativePath+ fileName + ".jh"])
+        subprocess.run([relativePath + cnExe, "-nc", relativePath + alterFullFileName, relativePath + fileName + ".jh"])
 
         # ksExe = "EXE\\kscode.exe"
         ksExe = "kscode.exe"
         # ksArgument = "-jk " + fileName + ".jh" + self.KS_DIC_FOLDER + "\\" + folderName + "\\" + fileName + '.txt'
         # process run kscode.exe (argument is ksArgument)
-        subprocess.run([relativePath + ksExe, "-jk", relativePath + fileName + ".jh", relativePath + self.KS_DIC_FOLDER + "\\" + folderName + "\\" + fileName + '.txt'])
+        subprocess.run([relativePath + ksExe, "-jk", relativePath + fileName + ".jh",
+                        relativePath + self.KS_DIC_FOLDER + "\\" + folderName + "\\" + fileName + '.txt'])
 
         # FILE delete fileName+".jh"
         removeFileName = relativePath + fileName + ".jh"
-        if os.path.isfile(removeFileName) :
+        if os.path.isfile(removeFileName):
             os.remove(removeFileName)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     testClass = lexicalDic()
     # testClass.makeGenericDB()
     testData = {'word': 'test'}
-    print(testClass.makeLexicalDB())
-    
+    print(testClass.probSearch(testData))
